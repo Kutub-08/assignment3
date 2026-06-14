365a9017c94be8b3c9c05ba999180fac82a5a823
Kutub-08 <kutubuddin2003251251@gmail.com>
2026-06-15 01:50:40 +0600

    Create Users table

--- QUERY.sql
+++ QUERY.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE Users (
+  user_id serial PRIMARY KEY,
+  full_name varchar(100) NOT NULL,
+  email varchar(100) UNIQUE,
+  role varchar(50) CHECK (role IN ('Ticket Manager', 'Football Fan')),
+  phone_number varchar(25)
+);

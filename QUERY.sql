5e15f03742d42329d7bc48e109ad9b636475b71c
Kutub-08 <kutubuddin2003251251@gmail.com>
2026-06-15 01:51:26 +0600

    create matchs table

--- QUERY.sql
+++ QUERY.sql
@@ -5,3 +5,19 @@ CREATE TABLE Users (
   role varchar(50) CHECK (role IN ('Ticket Manager', 'Football Fan')),
   phone_number varchar(25)
 );
+
+
+CREATE TABLE Matches (
+  match_id serial PRIMARY KEY,
+  fixture text NOT NULL,
+  tournament_category varchar(255) NOT NULL,
+  base_ticket_price decimal(10, 2) NOT NULL CHECK (base_ticket_price >= 0),
+  match_status varchar(50) NOT NULL CHECK (
+    match_status IN (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  )
+);
